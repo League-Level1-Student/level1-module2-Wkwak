1326dbc81e4c6f1e6f79026c79faaad5d9e1da1e
William Kwak <league@wts14.attlocal.net>
2018-08-25 17:31:10 -0700

    Minion lesson

--- src/Minion.java
+++ src/Minion.java
@@ -1,4 +1,31 @@
 
 public class Minion {
+	private String name;
+	private int eyes;
+	private String color;
+	private String master;
 
+	public Minion() {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getEyes() {
+		return eyes;
+	}
+
+	public void setEyes(int eyes) {
+		this.eyes = eyes;
+	}
+
+	public String getColor() {
+		return color;
+	}
 }
